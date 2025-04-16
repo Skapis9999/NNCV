4320b8847f6b5cbc10eff046341153f1ae9adb80
Christos <xskapetis@gmail.com>
2025-04-16 19:28:29 +0200

    Cut butch size back to 64 due to memory issues

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -29,9 +29,9 @@ wandb login
 
 python3 train_transformer.py \
     --data-dir ./data/cityscapes \
-    --batch-size 128 \
+    --batch-size 64 \
     --epochs 100 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "attention--unet-training-pretrained-end-with-conv-scheduler-freeze-and-unfreeze-128batch-512px" \
+    --experiment-id "attention--unet-training-pretrained-end-with-conv-scheduler-freeze-and-unfreeze-64batch-512px" \

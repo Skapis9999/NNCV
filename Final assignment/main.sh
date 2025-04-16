2d729f0d8927a77dbd833f67b6e10d40325ae488
Christos <xskapetis@gmail.com>
2025-04-16 19:24:28 +0200

    Double batch size from 64 to 128 and pixels from 256 to 512

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -29,9 +29,9 @@ wandb login
 
 python3 train_transformer.py \
     --data-dir ./data/cityscapes \
-    --batch-size 64 \
+    --batch-size 128 \
     --epochs 100 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "attention--unet-training-pretrained-end-with-conv-scheduler-freeze-and-unfreeze" \
+    --experiment-id "attention--unet-training-pretrained-end-with-conv-scheduler-freeze-and-unfreeze-128batch-512px" \
